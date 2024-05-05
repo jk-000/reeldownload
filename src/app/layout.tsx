12863f3c97476d865e862c1ec3fb638b12326e98
jenil <155545232+jenilkakadiya7@users.noreply.github.com>
2024-05-05 10:30:05 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,13 +64,35 @@ export default function RootLayout({
         >
           <ReactQueryProvider>
             <Navbar />
+              <script type="text/javascript">
+	atOptions = {
+		'key' : 'f06bb6817d6a162b50e2c297644ad2bc',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/f06bb6817d6a162b50e2c297644ad2bc/invoke.js"></script>
             <main className="relative h-[calc(100vh-6rem)] overflow-y-auto px-2 sm:px-4">
               {children}
             </main>
+            <script async="async" data-cfasync="false" src="//pl23233200.highcpmgate.com/2cbb9db1a26635025fbf3df69d1d86ee/invoke.js"></script>
+<div id="container-2cbb9db1a26635025fbf3df69d1d86ee"></div>
             <Footer />
           </ReactQueryProvider>
         </ThemeProvider>
-        <script type='text/javascript' src='//pl23229241.highcpmgate.com/6f/98/e0/6f98e07ef4829be4f8cf42eb323c78ef.js'></script>
+      
+        <script type="text/javascript">
+	atOptions = {
+		'key' : 'decda1ba2fc254a8a65d34391cd20887',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/decda1ba2fc254a8a65d34391cd20887/invoke.js"></script>
       </body>
     </html>
   );
